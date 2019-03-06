135047451d26661d850f690bb77adf7f5779063e
Ram Maheshwari <ramdevukd@gmail.com>
2019-03-06 20:09:32 +0530

    adding files

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const Register = require("./controllers/register");
+const SignIn = require("./controllers/signin");
+const Profile = require("./controllers/profile");
+const Image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "test",
+    database: "smart_brain_db"
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/profile/:id", (req, res) => {
+  Profile.handleProfile(req, res, db);
+});
+
+app.post("/signin", (req, res) => {
+  SignIn.handleSignIn(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  Register.handleRegister(req, res, db, bcrypt);
+});
+
+app.put("/image", (req, res) => {
+  Image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  Image.handleApiCall(req, res);
+});
+
+app.listen(3100, () => {
+  console.log("app is running on port 3100");
+});
+
+/*app.get("/", (req, res) => {});*/
